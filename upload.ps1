d10dce371963cbd066a309b69329edfe9bf865af
PosetMage <mail@posetmage.com>
2024-02-04 10:38:47 +0800

    upload

--- upload.ps1
+++ upload.ps1
@@ -1,6 +1,9 @@
+
 git config --local user.name "PosetMage"
-git config --local user.email "posetmage@gmail.com"
+git config --local user.email "mail@posetmage.com"
 git remote set-url origin git@POM:PosetMage/Survey.git
+
+git submodule update --recursive --remote
 
 git pull
 git add .
